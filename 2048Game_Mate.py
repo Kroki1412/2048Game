0af36a0cc64e522fa840a21e51a4834581d0e8ac
Balázs Máté <balazsm86@gmail.com>
2016-10-20 11:47:02 +0200

    Modified Zerostagef() and Newelement()

--- 2048Game_Mate.py
+++ 2048Game_Mate.py
@@ -6,7 +6,7 @@ Stage=[[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
 
 # ========== Functions ==========
 
-def ZeroStagef():
+def ZeroStagef(ZeroStage):
     global Stage
     for i in range(4):
         for j in range(4):
@@ -14,18 +14,19 @@ def ZeroStagef():
                 ZeroStage[i][j] = 1
             if Stage[i][j] != 0:
                 ZeroStage[i][j] = 0
-    return i,j
+    return ZeroStage
+    # return i,j
 
-def Startposition():
-    global Stage
+def Startposition(stage):
+    # global Stage
     # start_row=random.choice(start_position)
     # start_column=random.choice(start_position)
     start_row1 = random.randint(0,3)
     start_column1 = random.randint(0,3)
     start_row2 = random.randint(0,3)
     start_column2 = random.randint(0,3)
-    Stage[start_row1][start_column1]=2
-    Stage[start_row2][start_column2]=2
+    stage[start_row1][start_column1]=2
+    stage[start_row2][start_column2]=2
     while start_row1==start_row2 and start_column1==start_column2:
         start_row1 = random.randint(0,3)
         start_column1 = random.randint(0,3)
@@ -37,15 +38,25 @@ def PlayGround(Stage):
     print (Stage[2][0], "\t", Stage[2][1], "\t", Stage[2][2], "\t", Stage[2][3])
     print (Stage[3][0], "\t", Stage[3][1], "\t", Stage[3][2], "\t", Stage[3][3])
 
-def Newelement(function):
+def Newelement(zerostage):
     #csak, ahol 0 van, amikor új Stage lesz
     global Stage
     start_row3 = random.randint(0,3)
     start_column3 = random.randint(0,3)
-    for i in range(4):
-        for j in range(4):
-            if ZeroStage[i][j] == 1:
-                Stage[start_row3][start_column3]=2
+    while zerostage[start_row3][start_column3] == 0:
+        start_row3 = random.randint(0,3)
+        start_column3 = random.randint(0,3)
+    Stage[start_row3][start_column3] = 2
+
+# def Newelement(function):
+#     #csak, ahol 0 van, amikor új Stage lesz
+#     global Stage
+#     start_row3 = random.randint(0,3)
+#     start_column3 = random.randint(0,3)
+#     for i in range(4):
+#         for j in range(4):
+#             if ZeroStage[i][j] == 1:
+#                 Stage[start_row3][start_column3]=2
 
 def move_up():
     global Stage
@@ -115,7 +126,7 @@ def move_left():
                     Stage[i][j-3]=Stage[i][j]
                     Stage[i][j]=0
 #???????????????
-def fusion_up():
+def fusion():
 #before step
     global Stage
     for i in range(4):
@@ -125,33 +136,32 @@ def fusion_up():
                     Stage[i][j]=Stage[i][j]*2
 
 def key_press():
-    # direction=input("Direction: ")
-    # direction = None
     while True:
         direction=input("Direction: ")
         if direction == "w":
             move_up()
+            Newelement(ZeroStagef(ZeroStage))
             PlayGround(Stage)
-            Newelement(ZeroStagef())
-            # fusion_up()
         elif direction == "s":
             move_down()
+            Newelement(ZeroStagef(ZeroStage))
             PlayGround(Stage)
-            Newelement(ZeroStagef())
         elif direction == "d":
             move_right()
+            Newelement(ZeroStagef(ZeroStage))
             PlayGround(Stage)
-            Newelement(ZeroStagef())
         elif direction == "a":
             move_left()
+            Newelement(ZeroStagef(ZeroStage))
             PlayGround(Stage)
-            Newelement(ZeroStagef())
-        else:
+        elif direction == "q":
             quit()
+        else:
+            print("Invalid command!")
 
 # ========== Main ==========
-Startposition()
-ZeroStagef()
+
+Startposition(Stage)
 PlayGround(Stage)
 key_press()
 
